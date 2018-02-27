a56a6bfe246abd222641a8f1e257c16f93bec8fd
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-02-27 12:31:02 -0600

    save java exercise 1

---
+++ src/HelloWorld.java
@@ -0,0 +1,22 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.print("Hello, World!");
+
+
+        int myFavoriteNumber;
+        myFavoriteNumber = 13;
+        System.out.print(myFavoriteNumber);
+
+//        String myString = "This is my first string";
+//        System.out.print(myString);
+//
+//        String myString = "T";
+//        System.out.print(myString);
+
+//        String myString = 3.14159;
+//        System.out.print(myString);
+
+
+    }
+}
+
