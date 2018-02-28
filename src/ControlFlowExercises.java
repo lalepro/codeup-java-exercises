1cd4d20d21e789fa50c01b409eb695e2d91a7fa1
Laura Prochaska <lauraleeprochaska@gmail.com>
2018-02-27 21:40:37 -0600

    save

--- src/ControlFlowExercises.java
+++ src/ControlFlowExercises.java
@@ -67,18 +67,19 @@ public class ControlFlowExercises {
 //
 //
 //        }
-//        String Fizz = " ", Buzz = "buzz";
+//        String Fizz = " ", Buzz = " ";
 //        for(int i=1; i<=100; i++)
 //        {
-//            if (i % 3 == 0) {
+//            if (i % 15 == 0){
+//            System.out.println("FizzzzzzBuzzzzz is " + " " + i);
+//        }
+//            else if (i % 3 == 0) {
 //                System.out.println("Fizz is: " + Fizz + " " + i);
 //            }
 //            else if (i % 5 == 0){
-//                System.out.println("Buzz is: " + Buzz + " " + i);
+//                System.out.println("Buzzzzzzz is: " + Buzz + " " + i);
 //            }
-//            else if (i % 15 == 0){
-//                System.out.println("FizzBuzz is " + " " + i);
-//            }
+//
 //        }
 
 //====+==+=+=+==+==+========3 Display a table of powers------
@@ -89,45 +90,48 @@ public class ControlFlowExercises {
 //        Only continue if the user agrees to.
 
 
-//        Scanner scan = new Scanner(System.in);
-//
-//        System.out.println("What number would you like to go up to? ");
-//
-//
-//        String userInput = scan.next();
-//        double input = Double.parseDouble(userInput);
-//
-//        System.out.println("number | Squared | cubed");
-//        System.out.println("------ | --------| ------");
-//
-//        for(int i = 1; i<= 10; i++) {
-//            if (userInput != " ") {
-//                System.out.println(input + "    |   " + (input * input) + "    |   " + (input * input * input));
-//            }
-//        }
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What number would you like to go up to? ");
+
+
+        String userInput = scan.next();
+        double input = Double.parseDouble(userInput);
+
+        System.out.println("number | Squared | cubed");
+        System.out.println("------ | --------| ------");
+
+        for(int i = 1; i<= 10; i++) {
+            if (userInput != " ") {
+                System.out.println(input + "    |   " + (input * input) + "    |   " + (input * input * input));
+            }
+        }
 
 //        4. Grades into letter grades
 
-        Scanner scan = new Scanner(System.in);
-        System.out.println("What is your grade? ");
-        String userGrade = scan.nextLine();
-        double grade = Double.parseDouble(userGrade);
+//        Scanner scan = new Scanner(System.in);
+//        System.out.println("What is your grade? ");
+//        String userGrade = scan.nextLine();
+//        double grade = Double.parseDouble(userGrade);
+//
+//        for (int i = 1; i <= 1; i++) {
+//            if (grade == 99 || grade == 100) {
+//                System.out.println("A+: 99-100");
+//            } else if (grade >= 88 && grade <= 98) {
+//                System.out.println("A: 88-98");
+//            } else if (grade >= 80 && grade <= 87) {
+//                System.out.println("B: 80-87");
+//            } else if (grade >= 67 && grade <= 79) {
+//                System.out.println("C: 79-67");
+//            } else if (grade >= 60 && grade <= 66) {
+//                System.out.println("D:60-66");
+//            } else if(grade <= 59 && grade >= 0) {
+//                System.out.println("F: 59 or below");
+//            }
+//        }
 
-        for (int i = 1; i <= 1; i++) {
-            if (grade == 99 || grade == 100) {
-                System.out.println("A+: 99-100");
-            } else if (grade >= 88 && grade <= 98) {
-                System.out.println("A: 88-98");
-            } else if (grade >= 80 && grade <= 87) {
-                System.out.println("B: 80-87");
-            } else if (grade >= 67 && grade <= 79) {
-                System.out.println("C: 79-67");
-            } else if (grade >= 60 && grade <= 66) {
-                System.out.println("D:60-66");
-            } else if(grade <= 59 && grade >= 0) {
-                System.out.println("F: 59 or below");
-            }
-        }
+
+
 
 
 
